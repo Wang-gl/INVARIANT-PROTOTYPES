5f17495379744816a3eae9ce920068d1f512109e
Wang-gl <55538885+Wang-gl@users.noreply.github.com>
2025-09-18 10:45:55 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,16 @@
+
+python main.py  --config config/emlip/EMLIP_EMA,cifar10,m500mbs64sbs10.yaml
+python main.py  --config config/emlip/EMLIP_EMA,cifar10,m1000mbs64sbs10.yaml
+python main.py  --config config/emlip/EMLIP_EMA,cifar10,m1000mbs64sbs10.yaml
+python main.py  --config config/emlip/EMLIP_EMA,cifar100,m1000mbs64sbs10.yaml
+python main.py  --config config/emlip/EMLIP_EMA,cifar100,m2000mbs64sbs10.yaml
+python main.py  --config config/emlip/EMLIP_EMA,cifar100,m5000mbs64sbs10.yaml
+python main.py --config config/emlip/EMLIP_EMA,tiny,m2000mbs64sbs10.yaml
+python main.py --config config/emlip/EMLIP_EMA,tiny,m5000mbs64sbs10.yaml
+python main.py --config config/emlip/EMLIP_EMA,tiny,m10000mbs64sbs10.yaml
+python main.py --config config/EMLIP_EMA,imagenet100,m2000mbs64sbs10.yaml
+python main.py --config config/EMLIP_EMA,imagenet100,m5000mbs64sbs10.yaml
+python main.py --config config/EMLIP_EMA,imagenet100,m10000mbs64sbs10.yaml
+
+
+
